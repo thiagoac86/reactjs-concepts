4b3391430abedec6a9e538caf84fe91a19c6e8db
Thiago Azevedo <thi.azev.dev@gmail.com>
2021-07-14 19:49:32 -0300

    implemented functions

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,41 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      setTasks([
+        ...tasks,
+        {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false
+        }
+      ]);      
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let newTasks = new Array<Task>();
+    tasks.forEach(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+      }
+      newTasks.push(task);
+    })
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    // let newTasks = new Array<Task>();
+    // tasks.forEach(task => {
+    //   if(task.id !== id){
+    //     newTasks.push(task);        
+    //   }
+    // })
+    // setTasks(newTasks);
+    const filteredTasks = tasks.filter(task => task.id !== id);      
+    setTasks(filteredTasks);
   }
 
   return (
